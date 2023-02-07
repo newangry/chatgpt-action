e9bfdaf175e9207d96b1ff9a4d73c51810de071c
wholiday777 <williamholiday777@gmail.com>
2023-02-07 19:31:34 +0800

    chore: 优化文案

--- dist/index.js
+++ dist/index.js
@@ -13676,18 +13676,21 @@ function genReviewPRSplitedPrompt(title, body, diff, limit) {
         if (dif.length > limit) {
           const header = diff.split("\n", 1)[0];
           const info = "这个改动有点大，这里省略掉。";
-          splits.push(`
-          此处代码改动如下，你是否有评审建议：
-          ${header}\n${info}`);
-        } else splits.push(dif);
+          splits.push(`${header}\n${info}`);
+        } else {
+          splits.push(dif)
+        }
       }
-      return cur;
+      return `此处代码改动如下，你是否有评审建议：
+      ${cur}`;
     });
 
   return {
     welcomePrompts: [
-      `这里有一个标题是 ${title} 的 GitHub pull request，现在假设你是它的代码评审人，可以么？`,
-      `接下来我将把 pull request 里的代码改动一条条告诉你。如果代码变化过多，我将省略掉改动内容并告知你。`,
+      `这里有一个 GitHub pull request，现在假设你是这个 pull request 的代码评审人，请你先阅读一下 pull request 的标题和内容，读完了告诉我。
+      标题是 ${title}，
+      内容是 ${body}`,
+      `接下来我将把 pull request 的改动按文件一条条告诉你。若改动过大，我将省略掉并告知你`,
     ],
     diffPrompts: splits,
     endPrompt: `基于以上信息，你能告诉我这个 pull request 存在什么问题吗？有何改动建议？`,
